8ed22d9d467aed42854d20e476e0c0a7074c10b1
Jean-Philippe Paradis <hexstream@gmail.com>
2012-04-11 18:48:13 -0400

    Exports.

--- package.lisp
+++ package.lisp
@@ -1,5 +1,41 @@
 (in-package #:cl-user)
 
 (defpackage #:lispy-format
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:~c
+           #:~repeat
+           #:~%
+           #:~&
+           #:~page
+           #:~cardinal
+           #:~ordinal
+           #:~roman
+           #:~old-roman
+           #:~radix
+           #:~d
+           #:~binary
+           #:~octal
+           #:~hex
+           #:~ffloat
+           #:~efloat
+           #:~float
+           #:~money
+           #:~a
+           #:~s
+           #:~w
+           #:~_
+           #:~block
+           #:~i
+           #:~tab
+           #:~rtab
+           #:~stab
+           #:~srtab
+           #:~justify
+           #:~downcase
+           #:~capitalize
+           #:~capitalize1
+           #:~upcase
+           #:~p
+           #:~^
+           #:~format))
 
